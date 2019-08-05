244b60d7594b987a429819f4d9c29c4955cb6b36
Oscar Shoudt <shoudto@gmail.com>
2019-08-05 22:12:23 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@counter = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@counter += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@counter += 1
+  end
+
+  def self.song_count
+     @@counter
+  end
+
+end
